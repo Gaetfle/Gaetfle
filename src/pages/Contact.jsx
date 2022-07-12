5ec940379ea070730c2fa1005b2703e42ab69d36
Nguyễn Thị Như Phương <92770267+nhuphuongs@users.noreply.github.com>
2022-07-12 17:31:30 +0700

    2nd commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,105 +3,52 @@ import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
 import logo from "../assets/images/res-logo.png";
 import { Link } from "react-router-dom";
 
+
 const Contact = () => {
-  return <div>
-    <Container>
-      <Row>
-        <Col lg="3" md="4" sm="6">
-          <div className=" footer__logo text-start">
-            <img src={logo} alt="logo" />
-            <h5>Tasty Treat</h5>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Nesciunt pariatur accusamus
-            </p>
-          </div>
-        </Col>
+  return (
+    <div>
+      <Container>
+        <Row className="mt-5">
+          <Col lg="6" md="6"> </Col>
+          <Col lg="6" md="6">
+            <div className="social__links d-flex align-items-center gap-4 justify-content-end">
+            </div>
+          </Col>
+        </Row>
 
-        <Col lg="3" md="4" sm="6">
-          <h5 className="footer__title">Delivery Time</h5>
-          <ListGroup className="deliver__time-list">
-            <ListGroupItem className=" delivery__time-item border-0 ps-0">
-              <span>Sunday - Thursday</span>
-              <p>10:00am - 11:00pm</p>
-            </ListGroupItem>
+        <Row>
+          <Col lg="3" md="4" sm="6">
+            <h5 className="contact__title">Working day</h5>
+            <span className="contact-item">Monday - Sunday</span>
+            <p className="contact-item">8.00 - 21.00</p>
+          </Col>
 
-            <ListGroupItem className=" delivery__time-item border-0 ps-0">
-              <span>Friday - Saturday</span>
-              <p>Off day</p>
-            </ListGroupItem>
-          </ListGroup>
-        </Col>
+          <Col lg="3" md="4" sm="6">
+            <h5 className="contact__title">Phone</h5>
+            <span className="contact-item">+84 123 456 789</span>
+          </Col>
 
-        <Col lg="3" md="4" sm="6">
-          <h5 className="footer__title">Contact</h5>
-          <ListGroup className="deliver__time-list">
-            <ListGroupItem className=" delivery__time-item border-0 ps-0">
-              <p>Location: ZindaBazar, Sylhet-3100, Bangladesh</p>
-            </ListGroupItem>
-            <ListGroupItem className=" delivery__time-item border-0 ps-0">
-              <span>Phone: 01712345678</span>
-            </ListGroupItem>
+          <Col lg="3" md="4" sm="6">
+             <h5 className="contact__title">Address</h5>
+             <span className="contact-item">227 Nguyen Van Cu, P4, Q5 </span>
+          </Col>
 
-            <ListGroupItem className=" delivery__time-item border-0 ps-0">
-              <span>Email: example@gmail.com</span>
-            </ListGroupItem>
-          </ListGroup>
-        </Col>
+          <Col lg="3" md="4" sm="6">
+            <h5 className="contact__title">Email</h5>
+            <span className="contact-item">gaetfle@gmail.com</span>
+          </Col>
+        </Row>
 
-        <Col lg="3" md="4" sm="6">
-          <h5 className="footer__title">Newsletter</h5>
-          <p>Subscribe our newsletter</p>
-          <div className="newsletter">
-            <input type="email" placeholder="Enter your email" />
-            <span>
-              <i className="ri-send-plane-line"></i>
-            </span>
-          </div>
-        </Col>
-      </Row>
-
-      <Row className="mt-5">
-        <Col lg="6" md="6">
-          <p className="copyright__text">
-            Copyright - 2022, website made by Muhibur Rahman. All Rights
-            Reserved.
-          </p>
-        </Col>
-        <Col lg="6" md="6">
-          <div className="social__links d-flex align-items-center gap-4 justify-content-end">
-            <p className="m-0">Follow: </p>
-            <span>
-              {" "}
-              <Link to="https://www.facebook.com/muhib160">
-                <i className="ri-facebook-line"></i>
-              </Link>{" "}
-            </span>
-
-            <span>
-              <Link to="https://github.com/muhib160">
-                <i className="ri-github-line"></i>
-              </Link>
-            </span>
-
-            <span>
-              {" "}
-              <Link to=" https://www.youtube.com/c/MuhibsTechDiary">
-                <i className="ri-youtube-line"></i>
-              </Link>{" "}
-            </span>
-
-            <span>
-              {" "}
-              <Link to=" https://www.linkedin.com/in/muhib160/">
-                <i className="ri-linkedin-line"></i>
-              </Link>{" "}
-            </span>
-          </div>
-        </Col>
-      </Row>  
-  </Container>
-</div>;
+        <Row className="mt-5">
+          <Col lg="6" md="6"> </Col>
+          <Col lg="6" md="6">
+            <div className="social__links d-flex align-items-center gap-4 justify-content-end">
+            </div>
+          </Col>
+        </Row>  
+      </Container>
+    </div>
+  )
 };
 
 export default Contact;
